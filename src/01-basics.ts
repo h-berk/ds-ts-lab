61a6296944084172ae1643660f565c6087cd919d
Hasan Berk <hasanberk50@gmail.com>
2023-09-13 10:24:24 +0100

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -4,6 +4,20 @@ interface Friend {
     age: number
 }
 
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string
+        extension: number
+    }
+}
+
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+  }
+
 const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
@@ -20,7 +34,7 @@ const friend1: Friend = {
   console.log(friends[1]);
   
   //   -------------------
-  const colleague1 = {
+  const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -29,7 +43,7 @@ const friend1: Friend = {
     },
   };
   
-  const colleague2 = {
+  const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -38,7 +52,7 @@ const friend1: Friend = {
     },
   };
   
-  const colleague3 = {
+  const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
@@ -46,7 +60,7 @@ const friend1: Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+  export const colleagues : ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
